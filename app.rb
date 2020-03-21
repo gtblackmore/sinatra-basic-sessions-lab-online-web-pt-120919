a212f0fc1c986fd4751398e29103956506a53d52
George Blackmore <gtblackmore@gmail.com>
2020-03-21 17:13:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   end
   
   post '/checkout' do
-    session["item"] = params[:item]
-    @session = session
+    @session["item"] = params[:item]
+    @session
   end
 end
